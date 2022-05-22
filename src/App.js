7b23f2f8f986f077e5a3f1479465e185c1efdde4
karandeepsingh-nexg <karandeepsingh@viewlift.com>
2022-05-22 13:50:59 +0530

    adding css loaders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import './app.css'
 
 function App() {
   return (<>
+  <div className="container">
   <h1>WEBPACK SESSION</h1>
+  <div className="card-container">
+   <div className="left-container"></div>
+   <div className="right-container"></div>
+  </div>
+  </div>
   </>
   );
 }
